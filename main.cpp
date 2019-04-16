fdc185c0ac58ab3166b5ebfa4bff3a3aa90f4513
Jordan Brandes <jordanbrandes@yahoo.com>
2019-04-16 14:55:51 -0500

    Update main.cpp
    Added multiple options in  game play and the ability to lose health points.

--- main.cpp
+++ main.cpp
@@ -8,6 +8,14 @@ class Player {
     string name; 
     int health, damage;
     
+    
+    
+};
+
+class Door {
+    
+    public: 
+    int damage;
 };
 
 int main ()
@@ -18,11 +26,41 @@ int main ()
     Player1.health = 3;
     Player1.damage = 0;
     
+    Door Door1;
+    Door Door2;
+    
+    Door2.damage = 1;
+    
+    int choice = 0;
+    
+    if (Player1.health == 0)
+    std::cout << "Game Over" << std::endl;
+    
+    if (Player1.damage >= 1)
+    std::cout << "Be careful, you have taken one point of damage." << std::endl;
+    
     std::cout << "Welcome to the game! Before we begin, please give me your name." << endl;
     std::cin >> Player1.name;
-    std::cout << "The game has now begun " << Player1.name << " make each move carefully. You only have three lives.";
+    std::cout << "The game has now begun " << Player1.name << " make each move carefully. You only have three lives." << endl;
+    std::cout << "You wake up in a dark room with a pistol in your hand. You spot two doors in the distance. Which do you enter? 1 or 2?" << std::endl;
+    std::cin >> choice;
+    
+    
+    switch (choice) {
+        case 1: 
+        std::cout << "You pick up a torch and find yourself in a large bedroom with a treasure chest. Do you open the chest?" << std::endl;
+        break;
+        
+        case 2: 
+        std::cout << "You find yourself in darkness for a few minutes before falling down the stairs. Take one point of damage." << std::endl;
+        Player1.damage = 1;
+        Player1.health = 2;
+        break;
 
-
+    };
+    
+    std::cout << "Be careful " << Player1.name << " there are dangerous things ahead." << std::endl;
+    
 
   return 0;
 }
